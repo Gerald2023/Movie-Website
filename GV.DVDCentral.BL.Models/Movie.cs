45f6f819527b7a4916eb0d10960d2bab636a800d
Gerald Vallejos Picado <gerald.vallejospicado3357@fvtc.edu>
2023-09-08 14:26:01 -0500

    Add project files.

---
+++ GV.DVDCentral.BL.Models/Movie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GV.DVDCentral.BL.Models
+{
+    public class Movie
+    {
+
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public double Cost { get; set; }
+
+        public int  RatingId { get; set; }
+
+        public int FormatId { get; set; }
+
+        public int DirectorId { get; set; }
+
+        public int InStkQty { get; set; }
+
+        public string? ImagePath { get; set; }
+
+
+
+    }
+}
---
+++ GV.DVDCentral.BL.Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GV.DVDCentral.BL.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public int UserId { get; set; }
+
+        public DateTime ShipDate { get; set; }
+
+
+    }
+}
